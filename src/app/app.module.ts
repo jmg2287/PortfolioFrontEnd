4a9a3caaad1aab60f1e7d9a669acc07ecbf86152
Jose Manuel Gil <giljm87@gmail.com>
2022-11-13 19:04:45 -0300

    Maquetado app estatico

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'', component: AboutComponent},
+      {path:'experience', component: ExperienceComponent},
+      {path:'education', component: EducationComponent},
+      {path:'projects', component: ProjectsComponent},
+      {path:'contact', component: ContactComponent},
+      {path:'**', component: PageNotFoundComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
